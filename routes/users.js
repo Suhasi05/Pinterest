11b22ea5068a3b74ffef2e9ca0d4283a120f6168
Suhasi <suhasikakani545@gmail.com>
2024-06-12 20:36:58 +0530

    MongoDBURl

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,16 @@ require('dotenv').config();
 const mongoURL = process.env.DB_URL;
 // const mongoURL = process.env.DB_URL_Local;
 
-// mongoose.connect(mongoURL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-mongoose.connect(mongoURL)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // 30 seconds
+  socketTimeoutMS: 45000, // 45 seconds
+};
+
+mongoose.connect('your_mongodb_connection_string', options)
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.log('Database connection error:', err));
 
 const userSchema = new mongoose.Schema({
   username: {
